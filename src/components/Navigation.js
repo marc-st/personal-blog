6086991fa7450319ad97204b573ebeb8630cf934
marc-st <marc.stevens99@gmail.com>
2018-02-09 14:45:05 +0000

    DRYing up css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,25 @@
-import React, {Component} from 'react';
-import '../stylesheets/navigation.css';
-import { Link, } from 'react-router-dom';
+import React, { Component } from "react"
+import "../stylesheets/navigation.css"
+import { Link } from "react-router-dom"
 
 class Navigation extends Component {
-  render() {
-    return(
-      <div className="navigation">
-        <ul>
-          <li><Link to ="/about"><img src={require("../images/info.svg")} alt="Info" /></Link> </li>
-          <li><Link to = "/posts"> <img src={require("../images/blog.svg")} alt="Blog" /> </Link> </li>
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="navigation">
+				<ul>
+					<li className="navIcon">
+						<Link to="/about">
+							<img src={require("../images/info.svg")} alt="Info" />
+						</Link>
+					</li>
+					<li className="navIcon">
+						<Link to="/posts">
+							<img src={require("../images/blog.svg")} alt="Blog" />
+						</Link>
+					</li>
+				</ul>
+			</div>
+		)
+	}
 }
-export default Navigation;
+export default Navigation
