ecde03ff5479431e9afd618452c52aa95a88cb48
marc-st <marc.stevens99@gmail.com>
2018-02-10 16:47:45 +0000

    cleaned up the transitions

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,36 +6,25 @@ import { spring, AnimatedSwitch } from 'react-router-transition'
 import { Home } from './Home'
 import { Posts } from './Posts'
 
-function mapStyles(styles) {
-	return {
-		opacity: styles.opacity,
-		transform: `scale(${styles.scale})`
-	}
-}
-
-// wrap the `spring` helper to use a bouncy config
-function bounce(val) {
+function glide(val) {
 	return spring(val, {
-		stiffness: 330,
-		damping: 22
+		stiffness: 174,
+		damping: 24
 	})
 }
-// child matches will...
-const bounceTransition = {
-	// start in a transparent, upscaled state
+
+const pageTransitions = {
 	atEnter: {
 		opacity: 0,
-		scale: 1.2
+		offset: 100
 	},
-	// leave in a transparent, downscaled state
 	atLeave: {
-		opacity: bounce(0),
-		scale: bounce(0.8)
+		opacity: glide(0),
+		offset: glide(-100)
 	},
-	// and rest at an opaque, normally-scaled state
 	atActive: {
-		opacity: bounce(1),
-		scale: bounce(1)
+		opacity: glide(1),
+		offset: glide(0)
 	}
 }
 
@@ -44,11 +33,12 @@ class Content extends Component {
 		return (
 			<div className="mainPosts">
 				<AnimatedSwitch
-					atEnter={bounceTransition.atEnter}
-					atLeave={bounceTransition.atLeave}
-					atActive={bounceTransition.atActive}
-					mapStyles={mapStyles}
-					className="route-wrapper"
+					className="switchRule"
+					{...pageTransitions}
+					mapStyles={styles => ({
+						opacity: styles.opacity,
+						transform: `translateX(${styles.offset}%)`
+					})}
 				>
 					<Route path="/about" component={Home} />
 					<Route path="/posts" component={Posts} />
