e9c9051f4803549418e51018793f422ec386975e
marc-st <marc.stevens99@gmail.com>
2018-02-15 13:44:37 +0000

    click header title to go home

--- src/components/Shared.js
+++ src/components/Shared.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import '../stylesheets/headfoot.css'
 import Navigation from './Navigation'
+import { Link } from 'react-router-dom'
+
+import '../stylesheets/headfoot.css'
 
 export const Header = () => (
 	<header className="header">
-		<h1 className="App-title">Marc Stevens</h1>
+		<Link to="/" className="App-title">
+			<h1>Marc Stevens</h1>
+		</Link>
 		<Navigation />
 	</header>
 )
